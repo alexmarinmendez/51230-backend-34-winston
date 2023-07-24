49aeffe5c4dd667830926eca2fab20b1d8e5bc6e
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-24 09:30:39 -0500

    feat: add transports depending of the environment

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,6 @@
 import winston from 'winston'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const custom = {
     levels: {
@@ -21,32 +23,32 @@ const custom = {
 
 winston.addColors(custom.colors)
 
-const logger = winston.createLogger({
-    levels: custom.levels,
-    transports: [
-        new winston.transports.Console({
-            level: 'fatal',
-            // format: winston.format.simple()
-            // format: winston.format.json()
-            // format: winston.format.combine(
-            //     winston.format.timestamp(),
-            //     winston.format.json()
-            // )
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        }),
-        new winston.transports.File({
-            filename: './logs/server-log.log',
-            level: 'fatal',
-            format: winston.format.simple()
-            // format: winston.format.combine(
-            //     winston.format.colorize(),
-            //     winston.format.simple()
-            // )
+const createLogger = env => {
+    if (env === 'PROD') {
+        return winston.createLogger({
+            levels: custom.levels,
+            transports: [
+                new winston.transports.File({
+                    filename: './logs/server-log.log',
+                    level: 'fatal',
+                    format: winston.format.simple()
+                })
+            ]
         })
-    ]
-})
+    } else {
+        return winston.createLogger({
+            levels: custom.levels,
+            transports: [
+                new winston.transports.Console({
+                    level: 'fatal',
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.simple()
+                    )
+                }),
+            ]
+        })
+    }
+}
 
-export default logger
+export default createLogger(process.env.ENVIRONMENT)
